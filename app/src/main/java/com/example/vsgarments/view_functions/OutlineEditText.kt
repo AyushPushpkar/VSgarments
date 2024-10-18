0c95e09689adf20350eb8544635420f42c66be34
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2024-10-18 13:35:17 +0530

    edit text view

---
+++ app/src/main/java/com/example/vsgarments/view_functions/OutlineEditText.kt
@@ -0,0 +1,42 @@
+package com.example.vsgarments.view_functions
+
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import com.example.vsgarments.fontInter
+import com.example.vsgarments.ui.theme.textcolorblue
+
+@Composable
+fun editText(
+    hint : String
+){
+    var text by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = text ,
+        onValueChange = {
+                newText->text = newText
+        },
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = textcolorblue,
+            unfocusedBorderColor = textcolorblue,
+            focusedLabelColor = textcolorblue,
+            cursorColor = textcolorblue,
+        ),
+        label = { Text(
+            text = hint,
+            fontFamily = fontInter,
+            fontWeight = FontWeight.Normal,
+            color = textcolorblue
+        ) },
+        modifier = Modifier
+
+    )
+}
