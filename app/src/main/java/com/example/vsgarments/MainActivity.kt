6c6bedeee1940250069e6391fc3f8575a3d74bb2
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2024-10-24 23:54:17 +0530

    bottom nav build

--- app/src/main/java/com/example/vsgarments/MainActivity.kt
+++ app/src/main/java/com/example/vsgarments/MainActivity.kt
@@ -11,43 +11,30 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.vsgarments.layout.EditProfileScreen
-import com.example.vsgarments.layout.LoginScreen
+import com.example.vsgarments.layout.HomeScreen
+import com.example.vsgarments.layout.Profile_Screen
+import com.example.vsgarments.layout.Signup_Screen
 import com.example.vsgarments.ui.theme.VSgarmentsTheme
+import com.example.vsgarments.ui.theme.textcolorgrey
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            /*
-            * VSgarmentsTheme {
+            VSgarmentsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+//                    EditProfileScreen(modifier = Modifier.padding(innerPadding))
+//                    Splash_Screen(modifier = Modifier.padding(innerPadding))
+//                    Signup_Screen(modifier = Modifier.padding(innerPadding))
+//                    HomeScreen(modifier = Modifier.padding(innerPadding))
+//                    LoginScreen(modifier = Modifier.padding(innerPadding))
+                    Profile_Screen(modifier = Modifier.padding(innerPadding))
                 }
-            * */
-
-
             }
         }
-    }
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VSgarmentsTheme {
-        Greeting("Android")
+        window.statusBarColor = textcolorgrey.hashCode()
     }
 }
+
+
---
+++ app/src/main/java/com/example/vsgarments/view_functions/Bottom_Nav.kt
@@ -0,0 +1,29 @@
+package com.example.vsgarments.view_functions
+
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+
+@Composable
+fun Bottom_bar(
+    item: List<Bottom_Nav_item>,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    onItemClick: (Bottom_Nav_item) -> Unit,
+) {
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    NavigationBar(
+        modifier = modifier,
+        containerColor = Color.Transparent ,
+        tonalElevation = 8.dp ,
+    ) {
+        item.forEach {
+            val selected = it.route == backStackEntry.value?.destination?.route
+        }
+    }
+}
