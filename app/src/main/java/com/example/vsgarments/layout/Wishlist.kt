ced1ffca5b04a641cfbf1230e39170f6a0a2adbc
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2024-12-20 14:57:35 +0530

    wishlist up

--- app/src/main/java/com/example/vsgarments/layout/Wishlist.kt
+++ app/src/main/java/com/example/vsgarments/layout/Wishlist.kt
@@ -1,8 +1,10 @@
 package com.example.vsgarments.layout
 
+import android.graphics.Paint.Align
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -21,29 +23,46 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.vsgarments.R
+import com.example.vsgarments.navigation.MainScreen
 import com.example.vsgarments.navigation.Screen
+import com.example.vsgarments.ui.theme.VSgarmentsTheme
 import com.example.vsgarments.ui.theme.appbackgroundcolor
 import com.example.vsgarments.ui.theme.fontBaloo
+import com.example.vsgarments.ui.theme.fontInter
+import com.example.vsgarments.ui.theme.rateboxGreen
 import com.example.vsgarments.ui.theme.textcolorblue
+import com.example.vsgarments.ui.theme.textcolorgrey
 import com.example.vsgarments.ui.theme.tintGreen
+import com.example.vsgarments.ui.theme.tintGrey
 import com.example.vsgarments.ui.theme.topbardarkblue
 import com.example.vsgarments.ui.theme.topbarlightblue
+import com.example.vsgarments.view_functions.blue_Button
 
 @Composable
 fun Wishlist(
@@ -128,43 +147,175 @@ fun Wishlist(
                         .fillMaxWidth()
                         .height(75.dp)
                         .background(tintGreen)
+                        .padding(
+                            start = 20.dp,
+                            end = 30.dp
+                        ),
+                    contentAlignment = Alignment.CenterStart
                 ) {
+                    var isChecked by remember { mutableStateOf(true) }
+                    Text(
+                        text = "Show in stock products only",
+                        color = Color(0xFF6188A0),
+                        fontFamily = fontInter,
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 15.sp
+                    )
+                    Box(
+                        modifier = Modifier
+                            .align(Alignment.CenterEnd)
+                    ) {
+                        Switch(
+                            modifier = Modifier
+                                .scale(0.8f),
+                            checked = isChecked,
+                            onCheckedChange = { isChecked = it },
+                            colors = SwitchColors(
+                                checkedThumbColor = tintGreen,
+                                checkedTrackColor = topbardarkblue,
+                                checkedBorderColor = Color.Transparent,
+                                checkedIconColor = Color.Red,
+                                uncheckedThumbColor = textcolorgrey,
+                                uncheckedTrackColor = topbardarkblue,
+                                uncheckedBorderColor = Color.Transparent,
+                                uncheckedIconColor = Color.Black,
+                                disabledCheckedThumbColor = Color.Black,
+                                disabledCheckedTrackColor = Color.Black,
+                                disabledCheckedBorderColor = Color.Black,
+                                disabledCheckedIconColor = Color.Black,
+                                disabledUncheckedThumbColor = Color.Black,
+                                disabledUncheckedTrackColor = Color.Black,
+                                disabledUncheckedBorderColor = Color.Black,
+                                disabledUncheckedIconColor = Color.Black
+                            ),
+                            interactionSource = remember { MutableInteractionSource() }
+                        )
+                    }
                 }
-                
-                Spacer(modifier = Modifier.width(10.dp))
+                Spacer(modifier = Modifier.height(2.dp))
             }
 
             itemsIndexed(imageList.chunked(2)) { _, pair ->
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(appbackgroundcolor)
+                        .padding(vertical = 1.dp),
                     horizontalArrangement = Arrangement.SpaceEvenly
                 ) {
                     pair.forEach { imageitem ->
+                        val screenwidth = LocalConfiguration.current.screenWidthDp.dp - 6.dp
 
                         Column(
                             modifier = Modifier
-                                .background(color = Color.White)
+                                .background(Color.White)
+                                .width(screenwidth / 2)
                                 .padding(10.dp)
                         ) {
+
                             Image(
                                 painter = painterResource(id = imageitem.imageresId),
                                 contentDescription = null,
                                 modifier = Modifier
                                     .align(Alignment.CenterHorizontally)
-                                    .width(180.dp)
-                                    .height(300.dp)
-                                    .padding(10.dp),
+                                    .fillMaxWidth()
+                                    .height(250.dp)
+                                    .clip(
+                                        RoundedCornerShape(10.dp)
+                                    ),
                                 contentScale = ContentScale.Crop
                             )
 
+                            Spacer(modifier = Modifier.height(15.dp))
+
                             Text(
+                                modifier = Modifier
+                                    .padding(horizontal = 5.dp , vertical = 2.dp),
                                 text = imageitem.name,
-                                color = textcolorblue
+                                color = textcolorblue ,
+                                maxLines = 1
                             )
-                            Text(
-                                text = "${imageitem.rate}USD",
-                                color = textcolorblue
-                            )
+
+                            Row (
+                                modifier = Modifier ,
+                                verticalAlignment = Alignment.CenterVertically
+                            ){
+                                Image(
+                                    modifier = Modifier.align(Alignment.CenterVertically),
+                                    painter = painterResource(id = R.drawable.rounded_arrow_downward_24),
+                                    contentDescription = ""
+                                )
+                                Spacer(modifier = Modifier.width(1.dp))
+                                val percentless = percentLess(imageitem.ogprice , imageitem.currprice)
+                                Text(
+                                    modifier = Modifier.padding( vertical = 2.dp),
+                                    text = "${percentless}%",
+                                    color = rateboxGreen
+                                )
+                                Spacer(modifier = Modifier.width(4.dp))
+                                Text(
+                                    modifier = Modifier.padding(horizontal = 5.dp , vertical = 2.dp),
+                                    text = "${imageitem.ogprice}$",
+                                    color = tintGrey ,
+                                    textDecoration = TextDecoration.LineThrough
+                                )
+                                Spacer(modifier = Modifier.width(2.dp))
+                                Text(
+                                    modifier = Modifier.padding(horizontal = 5.dp , vertical = 2.dp),
+                                    text = "${imageitem.currprice}$",
+                                    color = textcolorblue
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Box(
+                                modifier = Modifier
+                                    .width(65.dp)
+                                    .height(25.dp)
+                                    .padding(horizontal = 5.dp)
+                                    .clip(
+                                        RoundedCornerShape(15.dp)
+                                    )
+                                    .background(rateboxGreen) ,
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Row (
+                                    modifier = Modifier
+                                        .padding(horizontal = 7.dp) ,
+                                    verticalAlignment = Alignment.CenterVertically
+                                ){
+                                    Text(
+                                        text = imageitem.rating.toString(),
+                                        color = Color.White,
+                                        fontFamily = fontBaloo,
+                                        fontWeight = FontWeight.SemiBold,
+                                        fontSize = 15.sp
+                                    )
+                                    Spacer(modifier = Modifier.width(2.dp))
+                                    Image(
+                                        modifier = Modifier.align(Alignment.CenterVertically),
+                                        painter = painterResource(id = R.drawable.rounded_star_half_24),
+                                        contentDescription = "",
+                                    )
+                                }
+                            }
+
+                            Box (
+                                modifier = Modifier
+                                    .align(Alignment.CenterHorizontally)
+                                    .padding(
+                                        top = 15.dp,
+                                        bottom = 5.dp
+                                    )
+                            ){
+                                blue_Button(
+                                    width_fraction = 0.8f,
+                                    button_text = "Add to Cart" ,
+                                    font_Family = fontBaloo
+                                ) {
+
+                                }
+                            }
+                            Spacer(modifier = Modifier.height(10.dp))
                         }
                     }
                 }
@@ -173,103 +324,45 @@ fun Wishlist(
     }
 }
 
-val imageList = listOf(
-    ImageList(
-        R.drawable.bulk_order,
-        300,
-        "Aryan"
-    ),
-    ImageList(
-        R.drawable.bulk_order,
-        300,
-        "Aryan"
-    ),
-    ImageList(
-        R.drawable.test,
-        300,
-        "Aryan"
-    ),
-    ImageList(
-        R.drawable.custom,
-        300,
-        "Aryan"
-    ),
-    ImageList(
-        R.drawable.custom,
-        300,
-        "Aryan"
-    ),
-    ImageList(
-        R.drawable.custom,
-        300,
-        "Aryan"
-    ),
-    ImageList(
-        R.drawable.custom,
-        300,
-        "Aryan"
-    ),
-    ImageList(
-        R.drawable.custom,
-        300,
-        "Aryan"
-    ),
-    ImageList(
-        R.drawable.custom,
-        300,
-        "Aryan"
-    ),
-    ImageList(
-        R.drawable.custom,
-        300,
-        "Aryan"
-    ),
-    ImageList(
-        R.drawable.custom,
-        300,
-        "Aryan"
-    ),
-    ImageList(
-        R.drawable.custom,
-        300,
-        "Aryan"
-    ),
-    ImageList(
-        R.drawable.custom,
-        300,
-        "Aryan"
-    ),
-    ImageList(
-        R.drawable.custom,
-        300,
-        "Aryan"
-    ),
-    ImageList(
-        R.drawable.custom,
-        300,
-        "Aryan"
-    ),
-    ImageList(
-        R.drawable.custom,
-        300,
-        "Aryan"
-    ),
-    ImageList(
-        R.drawable.custom,
-        300,
-        "Aryan"
-    ),
-    ImageList(
-        R.drawable.custom,
-        300,
-        "Aryan"
-    ),
+fun percentLess(
+    ogprice: Int,
+    currprice: Int
+): Int {
+    var percentage : Int
+    percentage = (ogprice - currprice) * 100 / ogprice
+    return percentage
+}
 
+val imageList = listOf(
+    ImageList(R.drawable.bulk_order, 300, 400 ,"Aryan" , 4.0f ),
+    ImageList(R.drawable.bulk_order, 300, 400 , "Aryan" , 4.0f ),
+    ImageList(R.drawable.bulk_order, 300, 400 ,"Aryan" , 4.0f ),
+    ImageList(R.drawable.test , 300 , 400 ,"Aryan" , 4.5f) ,
+    ImageList(R.drawable.custom, 300, 400 ,"Aryan" , 3.5f),
+    ImageList(R.drawable.custom, 300, 400 ,"Aryan" , 3.5f),
+    ImageList(R.drawable.custom, 300, 400 ,"Aryan" , 3.5f),
+    ImageList(R.drawable.custom, 300, 400 ,"Aryan" , 3.5f),
+    ImageList(R.drawable.custom, 300, 400 ,"Aryan" , 3.5f),
+    ImageList(R.drawable.test , 300 , 400 ,"Aryan" , 4.5f) ,
+    ImageList(R.drawable.custom, 300, 400 ,"Aryan" , 3.5f),
+    ImageList(R.drawable.custom, 300,400 , "Aryan" , 3.5f),
+    ImageList(R.drawable.custom, 300,400 , "Aryan" , 3.5f),
+    ImageList(R.drawable.custom, 300, 400 ,"Aryan" , 3.5f),
+    ImageList(R.drawable.custom, 300,400 , "Aryan" , 3.5f),
+    ImageList(R.drawable.custom, 300, 400 ,"Aryan" , 3.5f),
+    ImageList(R.drawable.custom, 300, 400 ,"Aryan" , 3.5f),
+    ImageList(R.drawable.test , 300 , 400 ,"Aryan" , 4.5f) ,
+    ImageList(R.drawable.custom, 300,400 , "Aryan" , 3.5f),
+    ImageList(R.drawable.custom, 300,400 , "Aryan" , 3.5f),
     )
 
 data class ImageList(
     val imageresId: Int,
-    val rate: Int,
+    val currprice: Int,
+    val ogprice : Int ,
     val name: String,
+    val rating : Float
 )
+
+
 
--- app/src/main/java/com/example/vsgarments/navigation/Bottom_Nav.kt
+++ app/src/main/java/com/example/vsgarments/navigation/Bottom_Nav.kt
@@ -26,6 +26,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
@@ -36,15 +37,18 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.vsgarments.layout.CategoryScreen
 import com.example.vsgarments.layout.HomeScreen
 import com.example.vsgarments.layout.Order_Screen
+import com.example.vsgarments.ui.theme.VSgarmentsTheme
 import com.example.vsgarments.ui.theme.topbardarkblue
 import com.example.vsgarments.ui.theme.topbarlightblue
 import com.example.vsgarments.view_functions.BottomBarShape
@@ -92,6 +96,7 @@ fun BottomNavBar(
 
     val backStackEntry = navController.currentBackStackEntryAsState()
     val middleIndex = items.size / 2
+    val lastIndex = items.size - 1
 
     val selectedItem = items.find { it.route == backStackEntry.value?.destination?.route }
         ?: items[middleIndex]
@@ -123,6 +128,7 @@ fun BottomNavBar(
         val horizontalPadding = 16.dp
         val screenWidth = LocalConfiguration.current.screenWidthDp.dp
         val tabWidth = (screenWidth - horizontalPadding * 2) / items.size
+        val width = (screenWidth )/2
         val circleOffsetX = (circleOffset.value * tabWidth.value - tabWidth.value).dp
 
         Card(
@@ -136,8 +142,7 @@ fun BottomNavBar(
             colors = CardDefaults.cardColors(Color.Transparent)
         ) {
             val density = LocalDensity.current.density
-            val cutoutCenterPx = (tabWidth.value * density) * (cutoutCenterOffset.value + 0.625f)
-
+            val cutoutCenterPx = (tabWidth.value* density)* (cutoutCenterOffset.value + 0.62f)
             Row(
                 modifier = Modifier
                     .height(70.dp)
@@ -230,3 +235,5 @@ fun BottomNavBar(
         }
     }
 }
+
+
