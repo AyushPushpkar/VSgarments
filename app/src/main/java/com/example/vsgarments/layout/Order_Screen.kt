9e4051b99538205b4880578fbd16cc167aa72539
Aryan Kumar <aryankumarchallenger4@gmail.com>
2024-12-23 18:47:54 +0530

    order screen

--- app/src/main/java/com/example/vsgarments/layout/Order_Screen.kt
+++ app/src/main/java/com/example/vsgarments/layout/Order_Screen.kt
@@ -1,4 +1,7 @@
 package com.example.vsgarments.layout
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,14 +14,33 @@ import com.example.vsgarments.ui.theme.appbackgroundcolor
 
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
@@ -43,16 +65,211 @@ import com.example.vsgarments.view_functions.blue_Button
 fun Order_Screen(
     modifier: Modifier ,
     navController: NavController
-){
+) {
 
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(appbackgroundcolor),
-    ){
-        AppTopBar(navController = navController)
+    ) {
+        Column {
+
+
+            AppTopBar(navController = navController)
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                item {
+                    Box(
+                        modifier = Modifier
+                            .background(color = Color.White)
+                            .fillMaxWidth(),
+                        contentAlignment = Alignment.Center
+                    ) {
+
+
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(10.dp)
+                                .offset(x = 10.dp),
+                            verticalArrangement = Arrangement.SpaceEvenly
+                        ) {
+                            Text(
+                                text = "My Order",
+                                color = Color.Black,
+                                modifier = Modifier.background(color = Color.Transparent)
+                            )
+                            Text(
+                                text = "SEARCH",
+                                color = Color.Black,
+                                modifier = Modifier.background(color = Color.Transparent)
+                            )
+                            Spacer(
+                                modifier = Modifier
+                                    .height(20.dp)
+                            )
+                            /*
+                            Row(modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(end = 15.dp),
+                                horizontalArrangement = Arrangement.SpaceBetween) {
+                                order("All")
+                                order("Shipped")
+                                order("Order")
+                                order("Delivered")
+                            }
+                            Spacer(
+                                modifier = Modifier
+                                    .height(10.dp)
+                            )
+                            Row(modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(end = 15.dp, bottom = 10.dp),
+                                horizontalArrangement = Arrangement.SpaceBetween) {
+                                order("Cancelled")
+                                order("Exchange")
+                                order("Return")
+                                Text("    ")
+                            }
+
+ */
+                          Orderbutton()
+
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
+
+/*
+*
+*
+@Composable
+fun order(name:String,modifier: Modifier = Modifier){
+
+    var isChecked by remember { mutableStateOf(false) }
+    val scale by animateFloatAsState(
+        targetValue = if(isChecked) 1.5f else 1f,
+        animationSpec = spring(dampingRatio = Spring.StiffnessMedium)
+    )
+    val tintColor = if(isChecked)Color.Blue else Color.Gray
+
+    Column(modifier = Modifier
+        .scale(scale)
+        .clickable { isChecked = !isChecked }) {
+        Box(modifier = Modifier
+            .background(color = tintColor, shape = CircleShape)
+            .padding(horizontal = 10.dp, vertical = 2.dp)) {
+            Text(
+                text = name
+            )
+        }
+
+    }
+}
+
+ */
+
+@Composable
+fun Orderbutton(){
+    var selectedbutton by remember { mutableIntStateOf(0) }
+    var selectedRow by remember { mutableStateOf("row1") }
+    val buttonlabes1 = listOf("All","Shipped","Order","Return")
+    val buttonlabes2 = listOf("Cancelled","Exchange","Delivered")
+    fun hb0(){
+
+    }
+    fun hb1(){
+
+    }
+    fun hb2(){
+
+    }
+    fun hb3(){
+
+    }
+    fun hb4(){
+
+    }
+    fun hb5(){
+
+    }
+    fun hb6(){
+
+    }
+
+    Column {
+        Row(modifier = Modifier
+            .padding(end = 10.dp)){
+            buttonlabes1.forEachIndexed{index, lable ->
+             val isSelected = selectedRow == "row1" && selectedbutton == index
+                val backgroundView = if(isSelected) Color.Cyan else Color.LightGray
+
+                Button(
+                    onClick = {
+                        selectedbutton = index
+                        selectedRow = "row1"
+                        if (index != 0){
+                            when (index){
+                                0->hb0()
+                                1->hb1()
+                                2->hb2()
+                                3->hb3()
+                            }
+                        }
+
+                    },
+                    colors = ButtonDefaults.buttonColors(backgroundView),
+                    modifier = Modifier
+                        .wrapContentWidth()
+                        .padding(horizontal = 3.dp)
+
+                ) {
+                    Text(
+                        text = lable
+                    )
+                }
+
+            }
+        }
+
+        Row(modifier = Modifier
+            .padding(end =  10.dp)){
+            buttonlabes2.forEachIndexed{index, lable ->
+                val isSelected = selectedRow == "row2" && selectedbutton == index
+                val backgroundView = if(isSelected) Color.Cyan else Color.LightGray
+
+                Button(
+                    onClick = {
+                        selectedbutton = index
+                        selectedRow = "row2"
+                        when (index){
+                            4->hb4()
+                            5->hb5()
+                            6->hb6()
+                        }
+                    },
+                    colors = ButtonDefaults.buttonColors(backgroundView),
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(3.dp)
+                ) {
+                    Text(
+                        text = lable
+                    )
+                }
+
+            }
+        }
+    }
+}
+
+
 
 
 
