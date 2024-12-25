4c54bf5053c81e579d4a1da7f117e832e040094f
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2024-12-25 20:17:53 +0530

    size selector final

--- app/src/main/java/com/example/vsgarments/view_functions/SizeSelector.kt
+++ app/src/main/java/com/example/vsgarments/view_functions/SizeSelector.kt
@@ -1,10 +1,13 @@
 package com.example.vsgarments.view_functions
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -24,10 +27,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import com.example.vsgarments.dataStates.ImageItem
 import com.example.vsgarments.ui.theme.appbackgroundcolor
+import com.example.vsgarments.ui.theme.tintGreen
 import com.example.vsgarments.ui.theme.tintGrey
 
 @Composable
@@ -49,14 +57,15 @@ fun SizeSelection(
                 .horizontalScroll(sizerowscroll),
             horizontalArrangement = Arrangement.spacedBy(6.dp)
         ) {
-            availableSizes.forEach { size ->
-                val isSelected = selectedSize == size
+            availableSizes.forEach { clothSize ->
+                val isSelected = selectedSize == clothSize
                 val backgroundColor = if (isSelected) appbackgroundcolor else Color.White
 
+                if (imageItem.sizeToStockMap[clothSize] == true)
                 Button(
                     onClick = {
-                        selectedSize = size
-                        imageItem.size = size
+                        selectedSize = clothSize
+                        imageItem.size = clothSize
                         imageItem.updatePriceBasedOnSize()
                         onSizeUpdated(imageItem)
                     },
@@ -65,9 +74,62 @@ fun SizeSelection(
                     modifier = Modifier
                         .wrapContentWidth()
                         .height(50.dp)
-                        .border(width = 1.dp, color = tintGrey, shape = RoundedCornerShape(10.dp))
+                        .border(
+                            width = 1.dp,
+                            color = tintGrey,
+                            shape = RoundedCornerShape(10.dp)
+                        )
                 ) {
-                    Text(text = size)
+                    val price = imageItem.sizeToPriceMap[clothSize]
+                    val currPrice = price?.first
+                    Column {
+                        Text(text = clothSize)
+//                        Text(text = "₹$currPrice")
+                    }
+                }
+                else{
+                    var buttonSize by remember { mutableStateOf(IntSize.Zero) }
+                    Button(
+                        onClick = {
+                            //toast
+                        },
+                        shape = RoundedCornerShape(10.dp),
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFD5D5D5)),
+                        modifier = Modifier
+                            .wrapContentWidth()
+                            .height(50.dp)
+                            .border(
+                                width = 1.dp,
+                                color = tintGrey,
+                                shape = RoundedCornerShape(10.dp)
+                            )
+                            .onGloballyPositioned { coordinates ->
+                                buttonSize = coordinates.size
+                            }
+                    ) {
+                        Box(modifier = Modifier
+                            .fillMaxSize()
+                            .drawBehind {
+                                drawLine(
+                                    color = tintGrey,
+                                    start = Offset(
+                                        -5 * size.width,
+                                        size.height / 2
+                                    ),
+                                    end = Offset(
+                                        5 * size.width,
+                                        size.height / 2
+                                    ),
+                                    strokeWidth = 1.dp.toPx()
+                                )
+                            }
+                        ) {
+                            Text(
+                                text = clothSize,
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
+                    }
                 }
             }
         }
