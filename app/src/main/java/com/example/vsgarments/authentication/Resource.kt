c124c76ffa4d531a440d3c911b2c648ec34effb9
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2024-12-30 17:19:52 +0530

    resource sealed class

---
+++ app/src/main/java/com/example/vsgarments/authentication/Resource.kt
@@ -0,0 +1,13 @@
+package com.example.vsgarments.authentication
+
+
+// for better structure
+sealed class Resource <T> (
+    val data : T? = null ,
+    val errorMassage : String? = null
+){
+    class Success<T>(data: T) : Resource<T>(data)
+    class Error<T>(message : String) : Resource<T>(errorMassage = message)
+    class Loading<T> : Resource<T>()
+    class Unspecified<T> : Resource<T>()
+}
