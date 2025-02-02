ffea6a9037006b74e67df777c0f33d7daf085751
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2025-02-02 13:43:36 +0530

    dec qty

--- app/src/main/java/com/example/vsgarments/cart/FirebaseCommon.kt
+++ app/src/main/java/com/example/vsgarments/cart/FirebaseCommon.kt
@@ -5,11 +5,11 @@ import com.example.vsgarments.authentication.util.Constants.USER_COLLECTION
 import com.example.vsgarments.dataStates.CartIItem
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import javax.inject.Inject
 
 class FirebaseCommon(
     private val firestore: FirebaseFirestore,
     private val auth: FirebaseAuth
-
 ) {
 
     private val cartCollection = firestore.collection(USER_COLLECTION).document(auth.uid!!).collection(CART_COLLECTION)
@@ -43,5 +43,28 @@ class FirebaseCommon(
         }.addOnFailureListener {
             onResult(null, it)
         }
+    }
+
+    fun decreaseQuantity(documentId: String, onResult: (String?, Exception?) -> Unit) {
+        firestore.runTransaction { transition -> // all the operations happens at once
+            val documentRef = cartCollection.document(documentId)
+            val document = transition.get(documentRef)
+            val productObject = document.toObject(CartIItem::class.java)
+
+            productObject?.let {
+                val newQuantity = it.quantity - 1
+                val newProductObject = it.copy(quantity = newQuantity)
+                transition.set(documentRef, newProductObject)
+
+            }
+        }.addOnSuccessListener {
+            onResult(documentId, null)
+        }.addOnFailureListener {
+            onResult(null, it)
+        }
+    }
+
+    enum class QuantityChanging {
+        INCREASE, DECREASE
     }
 }
