64558149e3438972d1c83265c8f75d05dcc5adf2
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2025-02-08 01:44:54 +0530

    fc addition

--- app/src/main/java/com/example/vsgarments/cart/FirebaseCommon.kt
+++ app/src/main/java/com/example/vsgarments/cart/FirebaseCommon.kt
@@ -2,6 +2,8 @@ package com.example.vsgarments.cart
 
 import com.example.vsgarments.authentication.util.Constants.CART_COLLECTION
 import com.example.vsgarments.authentication.util.Constants.USER_COLLECTION
+import com.example.vsgarments.authentication.util.Constants.WISHLIST_COLLECTION
+import com.example.vsgarments.wishlist.WishlistItem
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
@@ -11,6 +13,19 @@ class FirebaseCommon(
 ) {
 
     private val cartCollection = firestore.collection(USER_COLLECTION).document(auth.uid!!).collection(CART_COLLECTION)
+    private val wishlistCollection = firestore.collection(USER_COLLECTION).document(auth.uid!!).collection(WISHLIST_COLLECTION)
+
+    fun addProductToWishlist(wishlistItem: WishlistItem, onResult: (WishlistItem?, Exception?) -> Unit) {
+        wishlistCollection.document().set(wishlistItem)
+            .addOnSuccessListener {
+                onResult(wishlistItem, null)
+            }.addOnFailureListener {
+                onResult(
+                    null,
+                    it
+                )
+            }
+    }
 
     fun addProductToCart (cartProduct: CartIItem, onResult: (CartIItem?, Exception?) -> Unit) {
         cartCollection.document().set(cartProduct)
@@ -22,6 +37,24 @@ class FirebaseCommon(
                     it
                 )
             }
+    }
+
+    fun changeQuantity(quantity : Int , documentId: String, onResult: (String?, Exception?) -> Unit){
+        firestore.runTransaction { transition -> // all the operations happens at once
+            val documentRef = cartCollection.document(documentId)
+            val document = transition.get(documentRef)
+            val productObject = document.toObject(CartIItem::class.java)
+
+            productObject?.let {
+                val newProductObject = it.copy(quantity = quantity)
+                transition.set(documentRef, newProductObject)
+
+            }
+        }.addOnSuccessListener {
+            onResult(documentId, null)
+        }.addOnFailureListener {
+            onResult(null, it)
+        }
     }
 
     fun increaseQuantity(documentId: String, onResult: (String?, Exception?) -> Unit) {
